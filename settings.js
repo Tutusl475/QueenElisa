6b600f79f5ebffb3a08ef8249f156904134b6faf
Mr.Nadun <134408669+Nadunsl@users.noreply.github.com>
2024-08-25 08:58:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==QV3Mmd0VnU1ZzNMBXa4UkNGJWdRRHT6dTRtVzSVZzM5dVanFTTSpnbQNHcjo0QLNlQzV0Ypwd=30925318749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
